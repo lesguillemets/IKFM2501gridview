3e9970049e9c7ee2cbfd8548da2bb38f6b30f406
lesguillemets <kgkacko+github@gmail.com>
2025-01-08 01:21:58 +0900

    plott

--- plot_by_emotion.py
+++ plot_by_emotion.py
@@ -1,4 +1,4 @@
-from result import GResultDF, Emotion
+from result import GResultDF, Emotion, Cond2_Ref
 import pandas as pd
 from pandas import DataFrame
 
@@ -8,22 +8,27 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 def do_plot(data: list[DataFrame]):
-    fig = make_subplots(rows=2, cols=3)
+    fig = make_subplots(rows=2, cols=3, subplot_titles= [Emotion(i).name for i in range(5) ])
     df = pd.concat(map(lambda d: d.df, data))
     for i in range(5):
         col = 1+i % 3
         row = 1+i // 3
-        emo = Emotion(i)
+        emo: Emotion = Emotion(i)
         # https://stackoverflow.com/a/72588847/3026489
         dh = px.density_heatmap(
+                # df[(df['emotion'] == emo) & (df['condition2'] == Cond2_Ref.Otherr],
                 df[df['emotion'] == emo],
                 x = 'x',
-                y = 'y'
+                y = 'y',
+                text_auto=True,
+                nbinsx = 11,
+                nbinsy = 11,
+                title = emo.name
                 )
         for trace in range(len(dh["data"])):
             fig.append_trace(
                 dh["data"][trace],
-                row=row, col=col
+                row=row, col=col,
             )
     fig.show()
 
