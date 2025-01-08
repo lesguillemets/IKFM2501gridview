2be02e42bb3f4017a821b234c2658735990ce265
lesguillemets <kgkacko+github@gmail.com>
2025-01-08 12:23:46 +0900

    filtering beforehand

---
+++ filters.py
@@ -0,0 +1,45 @@
+from collections.abc import Callable
+from typing import Optional
+from pandas import DataFrame, Series
+
+from result import Cond2_Ref, Emotion
+
+
+type DataFilter = Callable[DataFrame, bool]
+
+class Funnel:
+    def __init__(self, f: Optional[DataFilter]=None):
+        if f is None:
+            fs = []
+        else:
+            fs = [f]
+        self.filters: list[DataFilter] = fs
+
+    @staticmethod
+    def new(f:Optional[DataFilter]=None):
+        return Funnel(f)
+
+    def and_then(self, f:DataFilter):
+        self.filters.append(f)
+        print(self.filters)
+        return self
+
+    def pour(self, df: DataFrame):
+        # FIXME: 多相すぎて返り値の annotation が面倒っぽい
+        if self.filters == []:
+            return df[:]
+        result = df
+        for f in self.filters:
+            # 多分これでいい…??
+            result = result[f(result)]
+        return result
+
+
+def ref_is_self(df: DataFrame) -> bool:
+    return df['condition2'] == Cond2_Ref.Self
+
+def ref_is_other(df: DataFrame) -> bool:
+    return df['condition2'] == Cond2_Ref.Other
+
+def emo_is(e: Emotion) -> DataFilter:
+    return (lambda df: df['emotion'] == e)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from result import GResultDF
+from result import GResultDF, Emotion
+import filters as fls
+
 import plot_by_emotion
 import argparse
 
@@ -11,7 +13,10 @@ def main():
     args = AP.parse_args()
     data_dir = args.dir or Path("./data")
     data_all = [GResultDF.from_file(f) for f in data_dir.glob("*.tsv")]
-    plot_by_emotion.do_plot(data_all)
+    funnel = fls.Funnel.new().and_then(fls.ref_is_self).and_then(fls.emo_is(Emotion.Hap))
+    print(funnel.pour(data_all[0].df))
+    print(data_all[0].df)
+    # plot_by_emotion.do_plot(data_all)
 
 
 if __name__ == "__main__":
